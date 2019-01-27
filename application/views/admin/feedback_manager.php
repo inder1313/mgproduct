2253198c16394dbe48d17e2fae1f05cbd8f4584c
sartaaj13 <inderjeet1313@outlook.com>
2019-01-27 19:03:51 +0530

    feedback manager

---
+++ application/views/admin/feedback_manager.php
@@ -0,0 +1,23 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+    <link rel="apple-touch-icon" href="<?=base_url()?>/css/favicon/mgfavicon.png">
+    <link rel="icon" type="image/png" href="<?=base_url()?>/css/favicon/mgfavicon.png">
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <link href="<?=base_url();?>css/admincss.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="content">
+        <div class="container-fluid">
+        	<div class="row">
+            </div>
+        </div>
+</div>    
+</body>
+</html>
